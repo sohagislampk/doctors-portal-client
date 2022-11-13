84c96409ed0edee31157c88ec8977c1ac35d5436
sohagislampk <sohagislampk@gmail.com>
2022-11-13 11:11:37 +0600

    Contact us Added

--- src/Pages/Home/ContactUs/ContactUs.js
+++ src/Pages/Home/ContactUs/ContactUs.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import cbg from '../../../assets/images/appointment.png'
 const ContactUs = () => {
     return (
-        <div>
-            <h1>g</h1>
+        <div style={{
+            background: `url(${cbg})`,
+            backgroundSize: 'cover'
+        }} className='text-center my-8 py-28'>
+            <h2 className='text-xl text-primary font-bold'>Contact Us</h2>
+            <p className='text-4xl text-white'>Stay Connect With Us</p>
+            <form className='flex justify-center flex-col just gap-4 my-4'>
+                <input type="text" placeholder="Email address" className="input mx-auto w-full max-w-xl" /><input type="text" placeholder="Subject" className="input mx-auto w-full max-w-xl" />
+                <input type="text" placeholder="Your Message" className="input mx-auto input-bordered input-lg w-full max-w-xl" />
+            </form>
         </div>
     );
 };
